f2c751ef2bbeba7cd2c4d7452332b8c79c3f05b9
Lara Pollet <lara_pollet@hotmail.com>
2023-03-30 18:32:06 +0200

    linearisatie test

--- Quad Copter/init.m
+++ Quad Copter/init.m
@@ -18,6 +18,8 @@ syms m L k b g kd Ixx Iyy Izz cm;
 % Izz = 10^(-2);
 % cm = 10^(4);
 
+
+
 %% equi point values
 psi_0 = 0;
 phi_0 = 0;
@@ -31,22 +33,42 @@ f2 = vy;
 f3 = vz;
 f4_a = -(kd/m)*vx; 
 f4_b = (kd*cm/m)*(sin(psi)*sin(phi)+cos(phi)*cos(psi)*sin(theta))*(v21+v22+v23+v24);
+
+%f4 = f4_a + f4_b;
+
 f5_a = -(kd/m)*vy;
 f5_b = (kd*cm/m)*(cos(phi)*sin(psi)*sin(theta)-cos(psi)*sin(theta))*(v21+v22+v23+v24);
-f6_a = -(kd/m)*vz -g;
+
+%f5 = f5_a + f5_b;
+
+f6_a = -(kd/m)*vz - g;
 f6_b = (kd*cm/m)*(cos(theta)*cos(phi))*(v21+v22+v23+v24);
+
+%f6 = f6_a + f6_b;
+
 f7 = wx + wy*(sin(phi)*tan(theta)) + wz*(cos(phi)*tan(theta));
 f8 = wy*cos(phi) - wz*sin(phi);
 f9 = sin(phi)/cos(theta)*wy + cos(phi)/cos(theta)*wz;
 f10_a =  -((Iyy- Izz)/Ixx)*wy*wz;
 f10_b = (L*k*cm/Ixx)*(v21- v23);
+
+%f10 = f10_a + f10_b;
+
 f11_a = -((Izz- Ixx)/Iyy)*wx*wz;
 f11_b = (L*k*cm/Iyy)*(v22- v24);
+
+%f11 = f11_a + f11_b;
+
 f12_a =  -((Ixx - Iyy)/Izz)*wx*wy;
 f12_b = (L*k*cm/Izz)*(v21- v21+ v24 - v24);
 
+%f12 = f12_a + f12_b;
+
+
+
+
 %% jacobiaan om de a matrix te maken
-J_a = jacobian([f1 f2 f3 f4_a f5_a f6_a f7 f8 f9 f10 f11 f12],[vx vy vz theta phi psi x y z wx wy wz]);
+J_a = jacobian([f1 f2 f3 f4_a f5_a f6_a f7 f8 f9 f10_a f11_a f12_a],[vx vy vz theta phi psi x y z wx wy wz]);
 
 %% jacobiaan om de B matrix te maken
 J_b = jacobian([0 0 0 f4_b f5_b f6_b 0 0 0 f10_b f11_b f12_b],[v21 v22 v23 v24]);
@@ -54,10 +76,20 @@ J_b = jacobian([0 0 0 f4_b f5_b f6_b 0 0 0 f10_b f11_b f12_b],[v21 v22 v23 v24])
 %% De equilibrium waardes in de Jacobiaan steken
 %maar omdat er geen wx wy wz equib waardes zijn is het nog steeds
 %nonlineair???
-Js_a = subs(J_a, [x y z theta psi phi], [0 0 0 0 0 0]); 
+Js_a = subs(J_a, [x y z theta psi phi, wx, wy, wz], [0 0 0 0 0 0 0 0 0]); 
 %A = double(Js_a);
-Js_b = subs(J_b, [x y z theta psi phi], [0 0 0 0 0 0]); 
+Js_b = subs(J_b, [x y z theta psi phi, wx, wy, wz], [0 0 0 0 0 0 0 0 0]);
 %B = double(Js_b);
 
 
-System = ss(Js_a,Js_b);
+
+
+
+A = double(subs(Js_a, [m L k b g kd Ixx Iyy Izz cm], [0.5 0.25 3*10^(-6) 10^(-7) 9.81 0.25 5*10^(-3) 5*10^(-3) 10^(-2) 10^(4)])); 
+B = double(subs(Js_b, [m L k b g kd Ixx Iyy Izz cm], [0.5 0.25 3*10^(-6) 10^(-7) 9.81 0.25 5*10^(-3) 5*10^(-3) 10^(-2) 10^(4)])); 
+C = [eye(3), zeros(3), zeros(3), zeros(3); zeros(3), zeros(3), eye(3), zeros(3)];
+D = zeros(6,4);
+
+
+
+%System = ss(Js_a,Js_b);
