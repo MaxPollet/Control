4c0aafa44203d816cb4b13789db49d1b5d95b0ba
Max Pollet <maxpollet@Maxs-MacBook-Pro.local>
2023-05-15 16:42:35 +0200

    the sinus of hell fixed

--- Die veer motor ding/test.m
+++ Die veer motor ding/test.m
@@ -1,7 +1,7 @@
-f = 0.8;
+f = 0.9;
 
 
-step = ((f-0.5)/0.1);
+step = round((f-0.5)/0.1);
 
 
 phi = 0;
@@ -11,18 +11,17 @@ if step == 0
 else
 
 
-    for i = 0:step
+    for i = 0:step-1
 
         phi_temp  = asin(sin(2*pi*(0.5+0.1*(i))*0.4+phi));
         if phi_temp > 0
-            if phi_temp == mod(2*pi*(0.5+0.1*(i))*0.4+phi,2*pi)
+            if abs(phi_temp - mod(2*pi*(0.5+0.1*(i))*0.4+phi,2*pi)) < 10^(-14)
                 phi = phi_temp;
-                
             else
                 phi = pi - phi_temp;
             end
         else
-             if (pi - phi_temp) == mod(2*pi*(0.5+0.1*(i))*0.4+phi,2*pi)
+             if abs((pi - phi_temp) - mod(2*pi*(0.5+0.1*(i))*0.4+phi,2*pi)) < 10^(-14)
                 phi = pi - phi_temp;
             else
                 phi = 2*pi - abs(phi_temp);
